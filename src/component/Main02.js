2f4f810ce918cda0c711e247b682c8e538fc04ec
PASKI0114 <123672338+PASKI0114@users.noreply.github.com>
2023-08-25 11:55:30 +0900

    업데이트완성

--- src/component/Main02.js
+++ src/component/Main02.js
@@ -60,6 +60,33 @@ function Create(props){
     </article>
 }
 
+function Upadte(props) {
+    //props내용을 변경하기 위해서 useState를 사용하자
+    const[title, setTitle] =useState(props.title);
+    const[body, setBody] =useState(props.body);
+
+    return<article>
+        <h2>Upadte</h2>
+        <form onSubmit ={(e)=>{
+            e.preventDefault();
+            const title = e.target.title.value; 
+            const body = e.target.body.value;
+            props.onUpdate(title,body);
+        }}> 
+            {/*props는 변경할수없다*/}
+            {/* <p><input type="text" name="title" value={props.title} /></p>
+            <p><textarea name="body" value={props.body}/></p> */}
+            <p><input type="text" name="title" value={title} onChange={(e)=>{
+                setTitle(e.target.value);
+            }}/></p>
+            <p><textarea name="body" value={body}onChange={(e)=>{
+                setBody(e.target.value);
+            }}/></p>
+            <p><input type="submit" value="Update" /></p>
+        </form>
+    </article>
+}
+
 export default function Main02(){
 
     const [top,setTop] = useState([
@@ -93,7 +120,13 @@ export default function Main02(){
         content = <Article title={title} body={body} />;
         //모드가 READ일때만 수정,삭제를 나오게하자
         //하나를 수정하기 위해서는 아이다기 팔요하다.
-        contextControl = <li><a href={/upadte/+id}>Upadte</a></li>
+       // contextControl = <li><a href={/update/+id}>Update</a></li>
+        contextControl=<>
+            <li><a href={"/update/"+id} onClick={(e)=>{
+                e.preventDefault();
+                setMode('UPDATE');
+            }}>Update</a></li>
+        </>
 
     }else if(mode === 'CREATE'){
         //컴포넌트(꺽쇠초록) 옆에 있는건 무조건 props
@@ -111,6 +144,37 @@ export default function Main02(){
             setId(nextId)
             setNextId(nextId+1)
         }}/>
+    }else if(mode ==='UPDATE'){
+        let title, body = null;
+        console.log(top);
+        console.log(id);
+        for (let i = 0; i < top.length; i++) {
+            console.log("top[i]:"+top[i].id+"id:"+id)
+            if (top[i].id === Number(id)) {
+                title = top[i].title;
+                body = top[i].body;
+            }
+            
+            content = <Upadte title={title} body ={body} onUpdate={(_title,_body)=>{
+                console.log(_title);
+                console.log(_body);
+                console.log(id);
+               // alert("update");
+               //원래내용 복사
+               const newTops = [...top];
+               //변경할 내용
+               const newUpdateTop = {id:Number(id), title:_title, body:_body};
+                for (let i = 0; i < newTops.length; i++) {
+                    if(newTops[i].id === Number(id)){
+                        //새로운 내용이 기존 내용을 덮어쓰기 하자
+                        newTops[i] = newUpdateTop;
+                        break;
+                    }
+                }
+                setTop(newTops);
+                setMode('READ');
+            }}></Upadte>
+        }
     }
 
     return(
